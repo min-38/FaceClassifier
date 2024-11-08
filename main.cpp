3983c8e8728d268d5cb6454e56f28a16218ee19f
min-38 <mindino03@gmail.com>
2024-11-08 20:58:08 +0900

    디렉토리에서 파일 불러와서 보여주기 기능 구현

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <filesystem>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+namespace fs = std::filesystem;
+using namespace cv;
+
+int main(int argc, char *argv[]) {
+    fs::path image_dir_path = "../images";
+
+    // 디렉터리가 존재하는지 확인
+    if (!fs::exists(image_dir_path))
+        cerr << "Image directory does not exist" << endl;
+
+    // 디렉터리의 모든 파일 나열하기
+    for (const auto& entry : fs::directory_iterator(image_dir_path)) {
+        imshow( entry.path(), imread( entry.path(), IMREAD_COLOR));
+        waitKey(1000); // 1초마다 사진 띄우기
+    }
+
     return 0;
 }
